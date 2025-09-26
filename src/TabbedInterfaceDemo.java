339aa297cd950689d15f61637c3c419e0add1362
Bigisaac03 <amanicibembe7@gmail.com>
2025-09-26 20:34:04 +0300

    Commit #1 Branch 1
    in the NV, on line 1 and 2, we imported javax,swing.* and jaa.awt.*;.  on line 4, we extended a class named TabbedInterfaceDemo to JFrame, on line 6, we created a constructor, and from 7-10, we customized the JFrame.

---
+++ src/TabbedInterfaceDemo.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TabbedInterfaceDemo extends JFrame {
+
+    public TabbedInterfaceDemo() {
+        this.setTitle("Tabbed Interface Demo");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 400);
+        this.setLocationRelativeTo(null);
+    }
+}
