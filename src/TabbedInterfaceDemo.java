953bc1b30f875faecaf545ad7fe2c8145ad7cc7d
Bigisaac03 <amanicibembe7@gmail.com>
2025-09-26 20:42:53 +0300

    Commit #2 Branch1
    in the NV , on line 14, we created a the tabbed pane, on 17-24, we Added the tabs to the pane, on 28-44, we set Tab 1: User Info (text fields and labels).

--- src/TabbedInterfaceDemo.java
+++ src/TabbedInterfaceDemo.java
@@ -8,5 +8,40 @@ public class TabbedInterfaceDemo extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(600, 400);
         this.setLocationRelativeTo(null);
+
+
+        // Create tabbed pane
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        // Add tabs
+        tabbedPane.addTab("User Info", createUserInfoPanel());
+        tabbedPane.addTab("Settings", createSettingsPanel());
+        tabbedPane.addTab("Statistics", createStatisticsPanel());
+        tabbedPane.addTab("Messages", createMessagesPanel());
+        tabbedPane.addTab("Help", createHelpPanel());
+
+        this.add(tabbedPane);
+        this.setVisible(true);
     }
+    
+    // Tab 1: User Info (text fields and labels)
+    public JPanel createUserInfoPanel() {
+        JPanel panel = new JPanel(new GridLayout(4, 2, 10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        panel.add(new JLabel("First Name:"));
+        panel.add(new JTextField());
+
+        panel.add(new JLabel("Last Name:"));
+        panel.add(new JTextField());
+
+        panel.add(new JLabel("Email:"));
+        panel.add(new JTextField());
+
+        panel.add(new JLabel("Phone Number:"));
+        panel.add(new JTextField());
+
+        return panel;
+    }
+
 }
